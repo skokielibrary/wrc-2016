03f705eb9f474a4de3161612c0a0861c59434590
brodieaustin <brodie.austin@gmail.com>
2015-12-07 22:54:36 -0600

    updates and added wow.js and animate.css through bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,38 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
 var browserSync = require('browser-sync');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['css', 'js'], function() {
+  browserSync.init({
+      server: "./dist"
+  });
 
-    browserSync.init({
-        server: "./dist"
-    });
-
-    gulp.watch("src/scss/*.scss", ['sass']);
-    gulp.watch("dist/*.html").on('change', browserSync.reload);
+  gulp.watch("src/scss/*.scss", ['css']);
+  gulp.watch("dist/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("src/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("dist/css"))
-        .pipe(browserSync.stream());
+  return gulp.src("src/scss/*.scss")
+    .pipe(sass())
+    .pipe(minifyCss())
+    .pipe(gulp.dest("src/css"))
 });
+
+gulp.task('css', ['sass'], function(){
+    return gulp.src(['bower_components/animate.css/animate.min.css', 'src/css/styles.css'])
+      .pipe(concat('styles.css'))
+      .pipe(gulp.dest("dist/css"))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('js', function(){
+  return gulp.src("bower_components/wow/dist/wow.min.js")
+    .pipe(gulp.dest("dist/js"))
+    .pipe(browserSync.stream());
+})
 
 gulp.task('default', ['serve']);
